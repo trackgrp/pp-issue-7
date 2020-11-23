00fcbe7c25398f30f3f795eb6716fa068bcfeca9
Matt d'Entremont <matt.dentremont@trackgrp.com>
2020-11-23 12:42:33 -0400

    Copy/paste code from Peter's sample repo

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -7,7 +7,12 @@ import { SelectedIndexChangedEventData, ValueList } from '@proplugins/nativescri
   templateUrl: './login.html',
 })
 export class LoginComponent implements OnInit {
-  public ngOnInit(): void {
+  public selectedIndex: number = null;
+  public hint = "My Hint";
+  public items: ValueList<string>;
+  public cssClass: string = "default";
+
+  public ngOnInit() {
     this.items = new ValueList<string>();
     for (let loop = 0; loop < 200; loop++) {
       this.items.push({
@@ -16,11 +21,6 @@ export class LoginComponent implements OnInit {
       });
     }
   }
-
-  public selectedIndex: number = null;
-  public hint = "My Hint";
-  public items: ValueList<string>;
-  public cssClass: string = "default";
 
   public onchange(args: SelectedIndexChangedEventData) {
     console.log(`Drop Down selected index changed from ${args.oldIndex} to ${args.newIndex}. New value is "${this.items.getValue(
